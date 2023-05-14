b9d1b209685d9600c1210315e3dedff226c0c92b
PeaceOloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-05-14 22:20:32 +0100

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int arr[5] = {1, 2, 3, 4, 5};
+
+  for (int i = 0; i < 5; i++) {
+    cout << "Element " << i << " is: " << arr[i] << endl;
+  }
+
+  return 0;
+}
